a3b5e9f9956f5475e1b8df14efe568e9c615cff3
Elais Player <elais@fastmail.com>
2020-06-24 21:18:57 -0600

    update dependencies

--- project.clj
+++ project.clj
@@ -1,32 +1,31 @@
-(defproject aerial.hanami "0.12.3"
+(defproject aerial.hanami "0.12.4"
   :description "Clojure(Script) client/server vega/vega-lite based dynamic interactive plots and charts"
   :url "https://github.com/jsa-aerial/hanami"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.439"]
-                 [org.clojure/core.async "0.4.474"]
-                 [org.clojure/data.csv "0.1.3"]
+                 [org.clojure/clojurescript "1.10.773"]
+                 [org.clojure/core.async "0.4.500"]
+                 [org.clojure/data.csv "1.0.0"]
 
                  [ring/ring-defaults "0.3.1"]
                  [bk/ring-gzip "0.2.1"]
-                 [ring-cljsjs "0.1.0"]
+                 [ring-cljsjs "0.2.0"]
 
                  [com.rpl/specter "1.1.3"]
 
                  [aerial.hanasu "0.2.4"]
 
                  [com.andrewmcveigh/cljs-time "0.5.2"]
-                 [re-com "2.5.0"]
-                 [reagent "0.8.1"]
+                 [re-com "2.8.0"]
+                 [reagent "1.0.0-alpha2"]
 
-                 [cljsjs/vega "5.4.0-0"]
-                 [cljsjs/vega-lite "3.4.0-0"]
-                 [cljsjs/vega-embed "4.0.0-0"]
-                 [cljsjs/vega-tooltip "0.18.1-0"]
-                 ]
+                 [cljsjs/vega "5.9.0-0"]
+                 [cljsjs/vega-lite "4.0.2-0"]
+                 [cljsjs/vega-embed "6.0.0-0"]
+                 [cljsjs/vega-tooltip "0.20.0-0"]                 ]
 
-  :plugins [[lein-figwheel "0.5.16"]
+  :plugins [[lein-figwheel "0.5.20"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
 
   :source-paths ["src/cljc" "src/clj" "src/cljs"]
